14029340a55063ea1063560eb847f60556e0f057
djcadaniel <djcadaniel@gmail.com>
2022-11-07 15:34:02 -0500

    starwars v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
+import { getCharacter, getPeople, getSearchCharacter } from './api/people';
 import './App.css';
 
 function App() {
+
+  const [people, setPeople] = useState([]);
+  const [currentCharacter, setCurrentCharacter] = useState(1)
+  const [errorState, setErrorState] = useState({hasError:false});
+  const [details, setDetails] = useState({});
+  const [textSearch, setTextSearch] = useState('');
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    getPeople(page)
+      .then(setPeople)
+      .catch(handleError);
+  }, [page]);
+
+
+  useEffect(() => {
+    getCharacter(currentCharacter)
+      .then(setDetails)
+      .catch(handleError)
+  }, [currentCharacter])
+  
+  
+  const handleError = (err)=>{
+    setErrorState({
+      hasError : true,
+      message: err.message
+    })
+  }
+
+  const showDetails =(character)=>{
+   const id = Number(character.url.split('/').slice(-2)[0]);
+   setCurrentCharacter(id);
+  }
+
+  const inputSearch = useRef();
+
+  const onChangeTextSearch =(event)=>{
+    event.preventDefault();
+    const text = inputSearch.current.value;
+    setTextSearch(text);
+  }
+
+  const onSearchSubmit =(event)=>{
+    if(event.key !== 'Enter') return;
+    inputSearch.current.value = '';
+    setDetails({});
+    getSearchCharacter(textSearch)
+      .then(setPeople)
+      .catch(handleError)
+  }
+
+  const onChangePage =(next)=>{
+    if(!people.previous && page + next <= 0) return;
+    if(!people.next && page + next >= 9) return;
+
+    setPage(page + next)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input 
+        type='text'
+        placeholder='Buscar un personaje'
+        ref = {inputSearch}
+        onChange = {onChangeTextSearch}
+        onKeyDown = {onSearchSubmit}
+      />
+      <div>
+        {errorState.hasError && <div>{errorState.message}</div> }
+        {people?.results?.map(character =>(
+          <ul key={character.name}>
+            <li onClick={()=>showDetails(character)}>{character.name}</li>
+          </ul>
+        ))}
+      </div>
+      <section>
+        <button onClick={()=>onChangePage(-1)} >Prev</button>| {page} |
+        <button onClick={()=>onChangePage(1)}>Next</button>
+      </section>
+      {
+        details && (
+          <aside>
+            <h1>{details.name}</h1>
+            <img src={`https://starwars-visualguide.com/assets/img/characters/${currentCharacter}.jpg`} alt={details.name} />
+            <ul>
+              <li>Altura: {details.height}</li>
+              <li>Masa: {details.mass}</li>
+              <li>Nacimiento: {details.birth_year}</li>
+            </ul>
+          </aside>
+        )
+      }
+    </>
   );
 }
 
---
+++ src/api/people.js
@@ -0,0 +1,31 @@
+export const getPeople = async(page)=>{
+  try{
+    const response = await fetch(`http://swapi.dev/api/people/?page=${page}`);
+    if(!response.ok){
+      throw new NetworkError();
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }catch(err){
+    throw err;
+  }
+}
+
+class NetworkError extends Error{
+  constructor(){
+    super("Network error");
+  }
+}
+
+export const getCharacter = async(id=1)=>{
+  const response = await fetch(`http://swapi.dev/api/people/${id}/`);
+  const data = response.json();
+  return data;
+}
+
+export const getSearchCharacter = async(name) =>{
+  const response = await fetch(`https://swapi.dev/api/people/?search=${name}`);
+  const data = await response.json();
+  return data;
+}
